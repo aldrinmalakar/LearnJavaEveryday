bb6e6bbdca6197d5f33e0e5171e89c257379068f
aldrinmalakar <aldrinmalakar@gmail.com>
2020-09-23 20:37:12 -0400

    Trying again

--- src/ClassWork092320/CheckEligibilityForVote.java
+++ src/ClassWork092320/CheckEligibilityForVote.java
@@ -1,2 +1,28 @@
-package ClassWork092320;public class CheckEligibilityForVote {
+package ClassWork092320;
+
+import java.util.Scanner;
+
+public class CheckEligibilityForVote {
+
+    public static void main(String[] args) {
+
+        CheckEligibilityForVote.eligibility();
+    }
+
+    /**
+    This Method will check the eligibility to vote
+    Author: Aldrin Malakar
+    Date: 09/23/20
+     */
+    public static void eligibility() {
+        Scanner age = new Scanner(System.in);
+        System.out.println("Please Enter Your Age Below: ");
+        int input = age.nextInt();
+
+        if (input>=18){
+            System.out.println("Congratulations!!! You Are eligible to vote.");
+        } else {
+            System.out.println("Sorry, you are still too young to vote!");
+        }
+    }
 }
--- src/ClassWork092320/FindOutLargestNumber.java
+++ src/ClassWork092320/FindOutLargestNumber.java
@@ -1,2 +1,38 @@
-package ClassWork092320;public class FindOutLargestNumber {
+package ClassWork092320;
+
+import java.util.Scanner;
+
+public class FindOutLargestNumber {
+
+    public static void methodToCall(){
+
+        Scanner num1 = new Scanner(System.in);
+        System.out.println("Enter Your First Number Of Choice ");
+        int input = num1.nextInt();
+
+        Scanner num2 = new Scanner(System.in);
+        System.out.println("Enter Your Second Number Of Choice ");
+        int input2 = num2.nextInt();
+
+        Scanner num3 = new Scanner(System.in);
+        System.out.println("Enter Your Third Number Of Choice ");
+        int input3 = num3.nextInt();
+
+        if( num1 >= num2 && num1 >= num3) {
+            System.out.println(num1 + " is the largest Number");
+
+        } else if (num2 >= num1 && num2 >= num3) {
+            System.out.println(num2 + " is the largest Number");
+
+        } else {
+            System.out.println(num3+" is the largest Number");
+    }
+
+
+
+    public static void main(String[] args) {
+
+    FindOutLargestNumber.methodToCall();
+
+    }
 }
